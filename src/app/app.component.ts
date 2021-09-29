d20d32a03ed4d5ab78e7ab27179a592e9e848cf9
Raghvender <raghvender@apptunix.com>
2021-09-29 10:17:51 +0530

    Translation Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,14 @@ export class AppComponent {
   title = 'landing';
   lati: number=40.7127753;
   long: number=-74.0059728;
+  isEng: Boolean = true;
+
+  changeLanguage(language){
+    if (language=='english'){
+      this.isEng = true;
+    } else {
+      this.isEng = false;
+    }
+    
+  }
 }
