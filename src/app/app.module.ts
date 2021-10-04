58241ba7a33ed764c3f03105e3a7b398f17b4df7
Raghvender <raghvender@apptunix.com>
2021-10-04 11:59:30 +0530

    Angular Material Package removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,6 @@ import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import { MatSlideToggleModule } from "@angular/material/slide-toggle";
-// import {MatSelectModule} from '@angular/material/select';
-// import {MatExpansionModule} from '@angular/material/expansion';
-// import {MatDatepickerModule} from '@angular/material/datepicker';
-// import {MatTableModule} from '@angular/material/table';
-// import {MatFormFieldModule} from '@angular/material/form-field';
-// import {MatPaginatorModule} from '@angular/material/paginator';
-// import { MatSortModule } from '@angular/material/sort';
-// import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
@@ -28,15 +19,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
       libraries: ["places", "drawing", "geometry"],
     }),
     BrowserAnimationsModule,
-    // MatSlideToggleModule,
-    // MatSelectModule,
-    // MatExpansionModule,
-    // MatDatepickerModule,
-    // MatTableModule,
-    // MatFormFieldModule,
-    // MatPaginatorModule,
-    // MatSortModule,
-    // MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent],
